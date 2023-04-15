20d1453910351028f85304c5d914c98d52bcc6e4
itzkrixh <krishna99405@gmail.com>
2023-04-15 12:11:33 +0530

    view and create orders
    Co-authored-by: aviiciii <laavesh1@gmail.com>

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -9,9 +9,9 @@ contract Escrow {
     struct ItemStruct {
         uint256 itemId;
         uint256 amount;
+        uint256 shipping_amount;
         uint256 timestamp;
         address seller;
-        Status status;
     }
 
     enum Status {
@@ -56,13 +56,38 @@ contract Escrow {
     } 
 
     // create items
-    function createItem(uint256 _amount) public {
+    function createItem(uint256 _amount, uint256 _shippingamount) public {
         totalItems++;
-        items[totalItems] = ItemStruct(totalItems, _amount, block.timestamp, msg.sender, Status.OPEN);
+        items[totalItems] = ItemStruct(totalItems, _amount, _shippingamount, block.timestamp, msg.sender);
     }
 
     // view items
-    function viewItem(uint256 _itemId) public view returns (uint256, uint256, uint256, address, Status) {
-        return (items[_itemId].itemId, items[_itemId].amount, items[_itemId].timestamp, items[_itemId].seller, items[_itemId].status);
+    function viewItem(uint256 _itemId) public view returns (uint256, uint256, uint256, uint256, address) {
+        return (items[_itemId].itemId, items[_itemId].amount, items[_itemId].shipping_amount, items[_itemId].timestamp, items[_itemId].seller);
     }
+
+
+    // order struct
+    struct OrderStruct {
+        ItemStruct item;
+        address buyer;
+        address seller;
+        address shipper;
+        Status status;
+    }
+
+    mapping(uint256 => OrderStruct) public orders;
+
+    // create order
+    function createOrder(uint256 _itemId) public {
+        orders[_itemId] = OrderStruct(items[_itemId], msg.sender, items[_itemId].seller, address(0), Status.OPEN);
+    }
+
+    // view order
+    function viewOrder(uint256 _itemId) public view returns (uint256, uint256, uint256, uint256, address, Status, address, address, address, Status) {
+        return (orders[_itemId].item.itemId, orders[_itemId].item.amount, orders[_itemId].item.shipping_amount, orders[_itemId].item.timestamp, orders[_itemId].item.seller, orders[_itemId].item.status, orders[_itemId].buyer, orders[_itemId].seller, orders[_itemId].shipper, orders[_itemId].status);
+    }
+
+
+
 }
