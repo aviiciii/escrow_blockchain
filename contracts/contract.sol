f1809bd8f497c0b2e870b085b1b1195351ff1a4d
aviiciii <laavesh1@gmail.com>
2023-04-28 10:48:32 +0530

    reorder code

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -14,6 +14,15 @@ contract Escrow {
         address seller;
     }
 
+    // order struct
+    struct OrderStruct {
+        ItemStruct item;
+        address buyer;
+        address seller;
+        address shipper;
+        Status status;
+    }
+
     enum Status {
         OPEN,
         CONFIRMED,
@@ -23,13 +32,11 @@ contract Escrow {
         DISPUTED_BY_SELLER // SELLER MISTAKE
     }
 
-    mapping(uint256 => ItemStruct) public items;
+    
 
 
     // state variables
 
-    address public buyer;
-    address public seller;
     address public shipper;
     address public escrow;
 
@@ -52,22 +59,24 @@ contract Escrow {
 
     
 
-    constructor (uint256 _escrowFee) {
+    constructor (uint256 _escrowFee, address _shipper) {
         // constructor
         escrow = msg.sender;
         escrowFeePercent = _escrowFee;
         // escrowFeePercent = 5;
 
+        // set the shipper address
+        shipper = _shipper;
 
 
     } 
 
-    // return total cost of item
-    function totalCost(uint256 _itemId) public view returns (uint256) {
-        // get cost of item
-        return items[_itemId].amount + items[_itemId].shipping_amount + (items[_itemId].amount * escrowFeePercent) / 100;
-    }
+    // ITEMS-------------------------------------------------------------
 
+    mapping(uint256 => ItemStruct) public items;
+    
+    // events
+    event ItemCreated(uint256 itemId, uint256 amount);
 
     // create items
     function createItem(uint256 _amount, uint256 _shippingamount) public {
@@ -75,6 +84,9 @@ contract Escrow {
         // calculate totalcost
 
         items[totalItems] = ItemStruct(totalItems, _amount, _shippingamount, block.timestamp, msg.sender);
+
+        // return success message with item and amount
+        emit ItemCreated(totalItems, _amount);
     }
 
     // view items
@@ -82,15 +94,12 @@ contract Escrow {
         return (items[_itemId].itemId, items[_itemId].amount, items[_itemId].shipping_amount, items[_itemId].timestamp, items[_itemId].seller);
     }
 
-
-    // order struct
-    struct OrderStruct {
-        ItemStruct item;
-        address buyer;
-        address seller;
-        address shipper;
-        Status status;
+    // get total cost of item
+    function totalCost(uint256 _itemId) public view returns (uint256) {
+        return items[_itemId].amount + items[_itemId].shipping_amount + (items[_itemId].amount * escrowFeePercent) / 100;
     }
+
+    // ORDERS-------------------------------------------------------------
 
     mapping(uint256 => OrderStruct) public orders;
 
@@ -98,35 +107,6 @@ contract Escrow {
     event OrderCreated(uint256 itemId, uint256 amount);
 
 
-    // create order on item purchase
-    function createOrder(uint256 _itemId) public payable {
-
-        require(items[_itemId].itemId > 0, "Item does not exist");
-        require(items[_itemId].itemId <= totalItems, "Item does not exist");
-
-        // calculate cost of the item + shipping + escrow fee
-        uint256 escrowFee = (items[_itemId].amount * escrowFeePercent) / 100;
-        uint256 total_cost = items[_itemId].amount + items[_itemId].shipping_amount + escrowFee;
-
-        require(msg.value == total_cost, string(abi.encodePacked("Incorrect amount sent, please send ", total_cost, " wei")));
-
-        // create order
-        orders[_itemId] = OrderStruct(items[_itemId], msg.sender, items[_itemId].seller, address(0), Status.OPEN);
-
-        // calculate buyer deposit
-        buyerDeposit = msg.value;
-
-        // add to escrow balance
-        escrowBalance += escrowFee;
-
-        // add shipping balance
-        shippingBalance += items[_itemId].shipping_amount;
-
-        // return success message with item and amount
-        emit OrderCreated(_itemId, msg.value);
-    }
-
-    // view order
     struct OrderDetails {
         uint256 itemId;
         uint256 amount;
@@ -138,6 +118,31 @@ contract Escrow {
         Status status;
     }
 
+
+    // create order on item purchase
+    function createOrder(uint256 _itemId) public payable {
+
+        require(items[_itemId].itemId > 0, "Item does not exist");
+        require(items[_itemId].itemId <= totalItems, "Item does not exist");
+
+        // calculate cost of the item + shipping + escrow fee
+        uint256 total_cost = totalCost(_itemId);
+
+        require(msg.value == total_cost, string(abi.encodePacked("Incorrect amount sent, please send ", total_cost, " wei")));
+
+        // create order
+        orders[_itemId] = OrderStruct(items[_itemId], msg.sender, items[_itemId].seller, address(0), Status.OPEN);
+
+        // calculate buyer deposit
+        buyerDeposit = msg.value;
+
+
+        // return success message with item and amount
+        emit OrderCreated(_itemId, msg.value);
+    }
+
+    // view order
+    
     function viewOrder(uint256 _itemId) public view returns (OrderDetails memory) {
         OrderDetails memory order = OrderDetails(
             orders[_itemId].item.itemId,
